8dc5aa39aa75f0459b0898798e733348e0586dc4
Michael Bialowas <mbialowas@rrc.ca>
2025-01-23 14:42:13 -0600

    Testing with Jest and mockups completed

--- src/api/v1/routes/itemRoutes.ts
+++ src/api/v1/routes/itemRoutes.ts
@@ -7,6 +7,6 @@ const router: Router = express.Router();
 router.get("/", itemController.getAllItems);
 router.post("/", itemController.createItem);
 router.put("/:id", itemController.updateItem);
-router.delete(":id", itemController.deleteItem);
+router.delete("/:id", itemController.deleteItem);
 
 export default router;
